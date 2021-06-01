aacb85419f8fb80f20f30c835aae4db48a9de61f
zitronenclara <clara.hertel@outlook.de>
2021-06-01 20:40:28 +0200

    ready event commented

--- src/eventHandler/events/ready.js
+++ src/eventHandler/events/ready.js
@@ -4,6 +4,13 @@ const Discord = require('discord.js')
 module.exports = {
 	name: 'ready',
 	once: true,
+	/**
+	 * Triggered when the Bot goes online
+	 *
+	 * @author: Clara
+	 * @param {Discord.Client} client The bot client
+	 * @since 1.0.0
+	 */
 	execute(client) {
 		console.log('Bot Onlin!');
 	},
